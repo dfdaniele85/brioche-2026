bbf0c282989398d187374dcedcb491eae32674cc
daniele <dfdaniele85@gmail.com>
2026-01-10 16:13:18 +0100

    UI: forza App.css globale (import in main.tsx)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+
+// Se hai altri css globali, lasciali QUI sopra (se esistono)
+// import "./index.css";
+
+// ✅ questo deve stare PER ULTIMO per avere priorità
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
